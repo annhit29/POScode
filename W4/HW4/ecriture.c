923ff1d60d5a7c6741b5bbf639d1a7248586de47
annhit29 <ann20010929@gmail.com>
2023-03-12 17:35:50 +0100

    ecriture.c aled

--- W4/HW4/ecriture.c
+++ W4/HW4/ecriture.c
@@ -9,7 +9,8 @@ int main(void){
     // le tableau prenom est utilisé pour stocker le nom entré par l'utilisateur à chaque itération de la boucle, un nom à la fois. Le tableau peut stocker jusqu'à TAILLE_NOM caractères pour chaque nom entré.
     char prenom[TAILLE_NOM +1]; //+1 pour le caractère de fin de chaîne '\0' (null character) qui est ajouté automatiquement par scanf
 
-    fichier = fopen(prenom, "w"); // ouverture du fichier en mode écriture (w = write)
+    // ouverture du fichier en mode écriture (w = write)
+    fichier = fopen(prenom, "w");
     // Jo      24 
     // Marc    35 
     // Ted     74 
@@ -17,6 +18,14 @@ int main(void){
     // Werner  48 
     // OldBob  103   
 
+    //ecriture dans le fichier avec iteration while (preferable):
+    do{
+        printf("Entrez un nom (CTRL+D pour terminer) :\n");
+        scanf("%s", prenom); // lecture du nom entré par l'utilisateur
+    }while(!feof(stdin) && !ferror(stdin)); // boucle jusqu'à ce que l'utilisateur tape CTRL+D (fin de fichier) pour terminer l'entrée des noms (CTRL+D est équivalent à EOF = End Of File)
+
+
+    fwrite(prenom, sizeof(char), TAILLE_NOM, fichier);
 
     return 0;
 }
